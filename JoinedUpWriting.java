bd382792ee324e929c5ab328d316e2079aae9ee7
littelspde <josh.k.mail@gmail.com>
2019-07-22 01:59:00 +1200

    Low level proof of concept - no Trie

--- JoinedUpWriting.java
+++ JoinedUpWriting.java
@@ -4,6 +4,22 @@ import java.util.Collections;
 
 public class JoinedUpWriting{
 
+    public static ArrayList<String> dictionary = new ArrayList<String>();
+
+    public static String findSingleChain(String searchString){
+
+        for(int i = 0; i < searchString.length() ; i++){
+            String target = searchString.substring(i);
+            for (String word : dictionary){
+                if (word.startsWith(target)){
+                    return word;
+                }
+            }
+        }
+        return "No match found";
+
+    }
+
     public static void main (String[] args){
 
         TreeNode testRoot = new TreeNode("Hello");
@@ -19,16 +35,21 @@ public class JoinedUpWriting{
         System.out.println(testNode.getParent());
         System.out.println(testNode.getChildren());
 
-        ArrayList dictionary = new ArrayList<String>();
         Scanner stdinReader = new Scanner(System.in);
 
         while (stdinReader.hasNextLine()){
+
             dictionary.add(stdinReader.nextLine());
         }
 
         System.out.println(dictionary);
-        Collections.sort(dictionary);
-        System.out.println(dictionary);
+        //Collections.sort(dictionary);
+        //System.out.println(dictionary);
+        System.out.println(findSingleChain("ogre"));
+        System.out.println(findSingleChain("this"));
+        System.out.println(findSingleChain("indic"));
+        System.out.println(findSingleChain("salmon"));
+        System.out.println(findSingleChain("ingrate"));
 
     }
 
